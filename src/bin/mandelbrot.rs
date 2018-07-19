5e1b66ac0fa2e4bf8f4c35357df64351a20bb5e3
lesguillemets <kgkacko+github@gmail.com>
2018-07-20 01:19:12 +0900

    threading for mandelbrot

--- src/bin/mandelbrot.rs
+++ src/bin/mandelbrot.rs
@@ -2,26 +2,52 @@ extern crate image;
 extern crate num;
 use num::complex::Complex;
 use std::cmp::max;
+use std::sync::mpsc;
+use std::thread;
 
 const MAX_ITER: u32 = 20000;
-const IMG_X: usize = 2000; // image width in pixels
-const IMG_Y: usize = 2000; // image height in pixels
-const XVIEWWIDTH: f64 = 3.0; // we take -1.5 to 1.5
-const YVIEWWIDTH: f64 = 3.0; // we take -1.5 to 1.5
+const IMG_X: usize = 5000; // image width in pixels
+const IMG_Y: usize = 5000; // image height in pixels
+const XVIEWWIDTH: f64 = 3.0; // we take -1.5 to 1.5 const YVIEWWIDTH: f64 = 3.0; // we take -1.5 to 1.5
+const YVIEWWIDTH: f64 = 3.0; // we take -1.5 to 1.5 const YVIEWWIDTH: f64 = 3.0; // we take -1.5 to 1.5
 const XLEFTMOST: f64 = -XVIEWWIDTH / 2.0;
 const YUPPERMOST: f64 = -YVIEWWIDTH / 2.0;
 const GRID_WIDTH: f64 = XVIEWWIDTH / (IMG_X as f64);
 const GRID_HEIGHT: f64 = YVIEWWIDTH / (IMG_Y as f64);
-const OUTSIDE: f64 = 4.0; // we take r^2 > 4.0 as outside in judging the convergence
+const DIVERGE_CRITERIA: f64 = 16.0; // we take r^2 > 16.0 as outside in judging the convergence
+
+const MAX_JOBS: usize = 8;
+const VERBOSE: bool = false;
 
 fn main() {
     let mut dat: Vec<u32> = vec![0; (IMG_X * IMG_Y) as usize];
-    for (i, p) in dat.iter_mut().enumerate() {
-        if let Some(n) = calc_val(get_loc(i)) {
-            *p = n;
+    let (tx, rx) = mpsc::channel();
+    for i in 1..MAX_JOBS {
+        let tx1 = mpsc::Sender::clone(&tx);
+        mk_calc_worker(tx1, i);
+    }
+    mk_calc_worker(tx, 0);
+
+    for (loc, result) in rx {
+        if let Some(n) = result {
+            if VERBOSE {
+                println!("{}\t{}", n, loc);
+            }
+            if let Some(p) = dat.get_mut(loc) {
+                *p = n;
+            }
         }
     }
+
     save(&dat);
+}
+
+fn mk_calc_worker(tx: mpsc::Sender<(usize, Option<u32>)>, modulo: usize) -> () {
+    thread::spawn(move || {
+        for i in (modulo..IMG_X * IMG_Y).step_by(MAX_JOBS) {
+            tx.send((i, calc_val(get_loc(i)))).unwrap();
+        }
+    });
 }
 
 fn get_loc(i: usize) -> Complex<f64> {
@@ -45,7 +71,7 @@ fn calc_val(c: Complex<f64>) -> Option<u32> {
     let mut z = Complex::new(0.0, 0.0);
     for iteration in 0..MAX_ITER {
         z = z * z + c;
-        if z.norm_sqr() > OUTSIDE {
+        if z.norm_sqr() > DIVERGE_CRITERIA {
             return Some(iteration + 1);
         }
     }
@@ -58,6 +84,9 @@ fn save(dat: &Vec<u32>) -> () {
 
 fn draw_picture(dat: &Vec<u32>) -> image::ImageBuffer<image::Rgba<u8>, Vec<u8>> {
     let maximum = dat.iter().fold(0, |m, &v| max(m, v));
+    if VERBOSE {
+        println!("MAX={}", maximum);
+    }
     let mut img_buf = image::ImageBuffer::new(IMG_X as u32, IMG_Y as u32);
     for (x, y, pixel) in img_buf.enumerate_pixels_mut() {
         let val = dat[x as usize + y as usize * IMG_X];
